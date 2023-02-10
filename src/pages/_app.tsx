21a2e5e578b1e32355a406cf6cfeee3cab42a52a
Jhonny Esquivel <jhonnynereos@gmail.com>
2023-02-10 12:58:33 -0500

    set up

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,34 +3,28 @@ import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 
 import { trpc } from "../utils/trpc";
-import {
-  ConnectionProvider,
-} from "@solana/wallet-adapter-react";
+import { ConnectionProvider } from "@solana/wallet-adapter-react";
 
 import "@solana/wallet-adapter-react-ui/styles.css";
 import "../styles/globals.scss";
 
 import ClientWalletProvider from "../contexts/ClientWalletProvider";
 import { useMemo } from "react";
-import "react-toastify/dist/ReactToastify.css"
-
+import "react-toastify/dist/ReactToastify.css";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
-
-  const endpoint = useMemo(() => process.env.NEXT_PUBLIC_RPC_NODE!, []);
-
-
+  const endpoint = useMemo(() => process.env.NEXT_PUBLIC_RPC_NODE || "", []);
 
   return (
     <ConnectionProvider endpoint={endpoint}>
-    <ClientWalletProvider>
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
-    </ClientWalletProvider>
+      <ClientWalletProvider>
+        <SessionProvider session={session}>
+          <Component {...pageProps} />
+        </SessionProvider>
+      </ClientWalletProvider>
     </ConnectionProvider>
   );
 };
