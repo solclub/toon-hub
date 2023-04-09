716ec1f9a1bd025bca3787dfaa491a6124734abb
Jhonny Esquivel <jhonnynereos@gmail.com>
2023-04-08 22:38:53 -0500

    removed traits images, avoid upload to cloudinary, reused buffer from preview and pass it to update metadata

---
+++ src/scripts/index.js
@@ -0,0 +1,44 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { config, uploader, v2 } = require("cloudinary");
+const fs = require("fs").promises;
+const path = require("path");
+
+// Configura Cloudinary con tu Cloud Name, API Key y Secret
+config({
+  cloud_name: "dmxtw3cxj",
+  api_key: "443312235294469",
+  api_secret: "vsANvhlJ6hhMVNoIdyfsJuzZ4Tw",
+});
+
+// CLOUDINARY_CLOUD_NAME=dmxtw3cxj
+// CLOUDINARY_API_KEY=443312235294469
+// CLOUDINARY_API_SECRET=vsANvhlJ6hhMVNoIdyfsJuzZ4Tw
+
+const rootDir = "../assets/traits";
+
+// Recorre la estructura de carpetas y sube todas las imágenes a Cloudinary
+const uploadImages = async (dir) => {
+  const files = await fs.readdir(dir);
+
+  for (const file of files) {
+    const filePath = path.join(dir, file);
+    const stats = await fs.stat(filePath);
+    if (stats.isDirectory()) {
+      await uploadImages(filePath);
+    } else {
+      const { name, dir: traitDir } = path.parse(filePath);
+      const [collection, upgradeType, traitName] = traitDir.split(path.sep).slice(-3);
+
+      const publicId = `collections/${collection}/art/${upgradeType}/${traitName}/${name}`;
+
+      await v2.uploader.upload(filePath, {
+        resource_type: "image",
+        public_id: publicId,
+        overwrite: false,
+      });
+      console.log(`Subida la imagen ${publicId}`);
+    }
+  }
+};
+
+uploadImages(rootDir).catch(console.error);
