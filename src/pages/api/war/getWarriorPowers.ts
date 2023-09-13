685a07531ff3e46a647d77bc552c6d4a4181530c
Arunkumar Sugadevan <arunbluez@gmail.com>
2023-09-13 23:46:07 +0200

    text fix api

--- src/pages/api/war/getWarriorPowers.ts
+++ src/pages/api/war/getWarriorPowers.ts
@@ -2,6 +2,11 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { getWarriorsPower } from "server/services/war-service";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "OPTIONS") {
+    // send status ok
+    res.status(200).end();
+    return;
+  }
   console.log("war api called", req.method);
   console.log(req.body);
   const { warriorList } = req.body;
