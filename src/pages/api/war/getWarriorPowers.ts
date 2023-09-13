9a589ddfda6465b5ea981a2cbd5b15b71000bdb7
Arunkumar Sugadevan <arunbluez@gmail.com>
2023-09-13 23:19:22 +0200

    text fix api

--- src/pages/api/war/getWarriorPowers.ts
+++ src/pages/api/war/getWarriorPowers.ts
@@ -2,10 +2,6 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { getWarriorsPower } from "server/services/war-service";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== "POST") {
-    res.status(405).send({ message: "Only POST requests allowed" });
-    return;
-  }
   console.log("war api called");
   console.log(req.body);
   const { warriorList } = req.body;
