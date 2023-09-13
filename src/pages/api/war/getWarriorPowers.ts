0cf6c567f3be129e64a743c8711df86238904b64
Arunkumar Sugadevan <arunbluez@gmail.com>
2023-09-13 23:57:27 +0200

    final fix api

--- src/pages/api/war/getWarriorPowers.ts
+++ src/pages/api/war/getWarriorPowers.ts
@@ -4,13 +4,11 @@ import { getWarriorsPower } from "server/services/war-service";
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "OPTIONS") {
     // send status ok
-    res.status(200).end();
+    res.status(204).end();
     return;
   }
-  console.log("war api called", req.method);
-  console.log(req.body);
   const { warriorList } = req.body;
-  console.log(warriorList);
+  console.log(warriorList.length);
   const totalPower = await getWarriorsPower(warriorList);
   console.log(totalPower);
   res.status(200).json({ totalPower: totalPower });
